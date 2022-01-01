77ed325db7e4749ebe9328830f0b51fd5b9fc9bd
spinsager <63341255+spinsager@users.noreply.github.com>
2022-01-02 00:13:34 +0530

    Docker configuration (#106)
    * Docker config

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { appRouter } from "./routes";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 const app: express.Application = express();
 app.disable("x-powered-by");
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.use("/", appRouter);
 
